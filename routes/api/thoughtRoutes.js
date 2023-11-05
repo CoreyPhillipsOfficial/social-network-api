fc7246bb3530cfa2df7f00950d592c27193ba72b
CoreyPhillipsOfficial <coreytphillips@icloud.com>
2023-11-04 20:13:06 -0400

    createThought

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -26,8 +26,7 @@ const {
     removeFriend,
 } = require('../../controllers/userController');
 
-router.route('/').get(findAllThoughts).post(createThought);
-router.route('/:userId').get(findUserById).put(updateUser).delete(deleteUser);
-router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend)
-
+router.route("/").get(findAllThoughts).post(createThought);
+router.route("/:userId").get(findUserById).put(updateUser).delete(deleteUser);
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 module.exports = router;
